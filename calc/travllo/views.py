610d80b8dc859539f2e004a00509ed4bf909d5d0
kunal <kunalkumar.kus@gmail.com>
2019-09-08 16:07:14 +0530

    #

---
+++ calc/travllo/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+    
+def about(request):
+    return render(request,'about.html')
+def blogs1(request):
+    return render(request,'blog-single.html')
+def blog(request):
+    return render(request,'blog.html')
+def contact(request):
+    return render(request,'contact.html')
+def destination(request):
+    return render(request,'destination.html')
+def hotel(request):
+    return render(request,'hotel-resto.html')
+
